403511e9f6e2c1af9ea19bb47f63fa36c042ebb0
Luca Martinelli <lucxmangajet@gmail.com>
2021-02-23 14:40:20 +0100

    added visual feedback for input

--- Assets/Scripts/Gameplay/Spaceship.cs
+++ Assets/Scripts/Gameplay/Spaceship.cs
@@ -14,25 +14,35 @@ public interface IDamagable
     void TakeDamage(Bullet bullet, float amount);
 }
 
-public class Spaceship : MonobehaviourPunPew, IDamagable
+public class Spaceship : MonobehaviourPunPew, IDamagable, IPunObservable
 {
     [SerializeField] float engineForce, rotationForce;
     [SerializeField] float maxHealth;
+    [SerializeField] float rotationCompensationMultiplyer;
 
     [Header("Components")]
     [SerializeField] SpriteRenderer teamColorsRenderer;
     [SerializeField] Transform cockpitCamTarget, weapon1CamTarget;
     [SerializeField] new Rigidbody2D rigidbody;
     [SerializeField] new Light2D light;
+    [SerializeField] ParticleSystem enginePS;
+    [SerializeField] Transform engineTransform;
+
+    [SerializeField] ParticleSystem rcsLeft, rcsRight;
 
     private Team owningTeam;
     private float health;
+
+    private float verticalCache;
+    private float horizontalCache;
+    private float rotationCompensation;
 
     public System.Action<Spaceship> HealthChanged;
 
     public Team Team { get => owningTeam; }
 
     public float Health { get => health; }
+
     public float MaxHealth { get => maxHealth; }
 
     protected override void OnAllReady()
@@ -56,16 +66,43 @@ public class Spaceship : MonobehaviourPunPew, IDamagable
                 photonView.RequestOwnership();
             }
         }
+
+        UpdateEffects();
+    }
+
+    private void UpdateEffects()
+    {
+        var emission = enginePS.emission;
+        emission.rateOverTimeMultiplier = Mathf.Lerp(10, 2000, Mathf.Abs(Mathf.Max(0,verticalCache)));
+
+        emission = rcsLeft.emission;
+        emission.rateOverTimeMultiplier = (Mathf.Lerp(0, 300, Mathf.Max(horizontalCache, -rotationCompensation / 20)));
+
+        emission = rcsRight.emission;
+        emission.rateOverTimeMultiplier = (Mathf.Lerp(0, 300, Mathf.Max(-horizontalCache, rotationCompensation / 20)));
     }
 
     private void PilotUpdate()
     {
-        var vertical = Input.GetAxis("Vertical");
-        var horizontal = Input.GetAxis("Horizontal");
+        verticalCache = Input.GetAxis("Vertical");
+        horizontalCache = Input.GetAxis("Horizontal");
 
         //Spaceship sprite is upsidedown, thats why there are "-"
-        rigidbody.AddTorque(rotationForce * -horizontal * Time.deltaTime);
-        rigidbody.AddForce(transform.up * -vertical * engineForce * Time.deltaTime);
+        rigidbody.AddTorque(rotationForce * -horizontalCache * Time.deltaTime);
+        rigidbody.AddForce(transform.up * Mathf.Min(-0.05f,-verticalCache) * engineForce * Time.deltaTime);
+
+        if (Mathf.Abs(horizontalCache) < 0.1f)
+        {
+            float rot = -rigidbody.angularVelocity;
+            rotationCompensation = rot;
+        }
+        else
+        {
+            rotationCompensation = 0;
+        }
+
+        rigidbody.AddTorque(rotationCompensation * rotationCompensationMultiplyer * Time.deltaTime);
+
     }
 
     public void Server_SetTeam(Team team)
@@ -115,6 +152,22 @@ public class Spaceship : MonobehaviourPunPew, IDamagable
         if (bullet.Team != owningTeam)
         {
             Server_SetHealth(health - amount);
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(horizontalCache);
+            stream.SendNext(verticalCache);
+            stream.SendNext(rotationCompensation);
+        }
+        else
+        {
+            horizontalCache = (float)stream.ReceiveNext();
+            verticalCache = (float)stream.ReceiveNext();
+            rotationCompensation = (float)stream.ReceiveNext();
         }
     }
 }
