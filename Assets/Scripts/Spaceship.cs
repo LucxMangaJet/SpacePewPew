f238d22d340a67a7302c858b91351fef47000ac8
Luca Martinelli <lucxmangajet@gmail.com>
2021-02-23 10:41:17 +0100

    switched to 2d lighting, added normal maps, added asteroid

--- Assets/Scripts/Spaceship.cs
+++ Assets/Scripts/Spaceship.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 //Maybe write own client side prediction: https://www.kinematicsoup.com/news/2017/5/30/multiplayerprediction
 
@@ -14,8 +15,10 @@ public class Spaceship : MonobehaviourPunPew
     [SerializeField] SpriteRenderer teamColorsRenderer;
     [SerializeField] Transform cockpitCamTarget, weapon1CamTarget;
     [SerializeField] new Rigidbody2D rigidbody;
+    [SerializeField] new Light2D light;
 
     private Team owningTeam;
+
 
     private void Update()
     {
@@ -63,6 +66,8 @@ public class Spaceship : MonobehaviourPunPew
 
         ServiceLocator.SetLocation(team, Location.Cockpit, cockpitCamTarget);
         ServiceLocator.SetLocation(team, Location.Weapon1, weapon1CamTarget);
+
+        light.gameObject.SetActive(team == GetLocalTeam());
     }
 
 }
