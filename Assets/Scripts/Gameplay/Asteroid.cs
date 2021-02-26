d95a0f9e10f3a24464d26dec5a0aac3e3a8e55e6
LucasBoe <lucas@lumotex.de>
2021-02-26 19:59:26 +0100

    asteroid now multiply when shooting at them

--- Assets/Scripts/Gameplay/Asteroid.cs
+++ Assets/Scripts/Gameplay/Asteroid.cs
@@ -3,12 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Asteroid : MonobehaviourPunPew
+public class Asteroid : MonobehaviourPunPew, IDamagable
 {
     [SerializeField] Vector2 startRotationMinMax;
     [SerializeField] new Rigidbody2D rigidbody;
     [SerializeField] Transform lightTransform;
     [SerializeField] Vector3 lightOffset;
+    [SerializeField] float hp = 40, maxHp = 40;
 
 
     protected override void OnAllReady()
@@ -23,5 +24,25 @@ public class Asteroid : MonobehaviourPunPew
     private void Update()
     {
         lightTransform.position = transform.position + lightOffset;
+    }
+
+    public void TakeDamage(Bullet bullet, float amount)
+    {
+        if (photonView.IsMine)
+        {
+            PhotonNetwork.Destroy(bullet.gameObject);
+
+            hp -= amount;
+
+            if (hp <= 0)
+            {
+                hp = maxHp;
+                transform.localScale = transform.localScale * 0.66f;
+                transform.position -= (Vector3.left * 0.25f);
+                string path = ServiceLocator.PREFABS_PATH + "Asteroid";
+                GameObject newAsteroid = PhotonNetwork.Instantiate(path, transform.position + Vector3.right * 0.5f, transform.rotation);
+                newAsteroid.transform.localScale = transform.localScale;
+            }
+        }
     }
 }
